bdf62b377dfb34cab1385d9950ba75453d62008a
Radhika <radhika84b@gmail.com>
2021-06-04 05:13:48 -0400

    Add files via upload

---
+++ My_First_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_First_ApplicationApp.swift
+//  My First Application
+//
+//  Created by Radhika Bhardwaj on 3/27/21.
+//
+
+import SwiftUI
+
+@main
+struct My_First_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
