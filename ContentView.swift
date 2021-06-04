5a3e7ba2db9b5c3d3b1be20b8b94e488d07b12c2
Radhika <radhika84b@gmail.com>
2021-06-04 05:14:58 -0400

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  My First Application
+//
+//  Created by Radhika Bhardwaj on 3/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
